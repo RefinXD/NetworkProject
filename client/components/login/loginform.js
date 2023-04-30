e7286f5f24672b05a8d7f12be85b0b8b53021039
AunchisaS <creamaunchisa09@gmail.com>
2023-04-30 23:30:01 +0700

    style login

--- client/components/login/loginform.js
+++ client/components/login/loginform.js
@@ -61,67 +61,68 @@ function Login() {
 
   return (
     <>
-    <header className="header">
-      <div className="projectName">
-        {/* <Link to="/">Chit Chat</Link> */}
+    <div className="header" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', backgroundColor: '#333', padding: '20px', color: '#fff',flexflow: 'row'}}>
+      <div className="projectName" style={{ fontSize: '2rem'}}>
+        Chit Chat
       </div>
-      <ul>
-        <li onClick={handleLogin}>  
-            <FaSignInAlt />
-            Login
-        </li>
-        <li onClick={handleRegister}>
-            <FaUser />
-            Register
-        </li>
-      </ul>
-    </header>
-      <section className="heading">
-        <h1>
-          <FaSignInAlt /> Login
-        </h1>
-        <p>Please login to get support</p>
-      </section>
-      <section className="form">
-        <form onSubmit={onSubmit}>
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control"
-              id="username"
-              name="username"
-              value={username}
-              onChange={onChange}
-              placeholder="Enter Your username"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              name="password"
-              value={password}
-              onChange={onChange}
-              placeholder="Enter Your password"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <button className="btn btn-block">Submit</button>
-          </div>
-          <div
-           style={{ alignSelf: "center", fontSize: "0.8rem", marginTop: "0.5rem" }}
-      >   
-        <label style={{ color: "gray" }}>Don&apos;t have an account? </label>
-        <div>
+  <ul style={{ display: 'flex', alignItems: 'center', listStyle: 'none', margin: 0}}>
+    <li onClick={handleLogin} style={{ listStyle: 'none', marginRight: '20px', cursor: 'pointer'}}>  
+      <FaSignInAlt />
+      <span style={{ marginLeft: '5px'}}>Login</span>
+    </li>
+    <li onClick={handleRegister} style={{ cursor: 'pointer'}}>
+      <FaUser />
+      <span style={{ marginLeft: '5px'}}>Register</span>
+    </li>
+  </ul> 
+</div>
 
+  
+    <div className="heading" style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>
+        <FaSignInAlt /> Login
+      </h1>
+      <p>Please login to get support</p>
+    </div>
+    <div className="form" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '50px' }}>
+      <form onSubmit={onSubmit} style={{ width: '100%', maxWidth: '400px' }}>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            name="username"
+            value={username}
+            onChange={onChange}
+            placeholder="Enter Your username"
+            required
+            style={{ width: '100%', padding: '10px', marginTop: '10px' }}
+          />
         </div>
-      </div>
-        </form>
-      </section>
-    </>
+        <div className="form-group">
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            placeholder="Enter Your password"
+            required
+            style={{ width: '100%', padding: '10px', marginTop: '10px' }}
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-block" style={{ backgroundColor: '#007bff', color: '#fff', padding: '10px', marginTop: '20px' }}>Submit</button>
+        </div>
+        <div style={{ alignSelf: 'center', fontSize: '0.8rem', marginTop: '0.5rem' }}>
+          <label style={{ color: 'gray' }}>Don&apos;t have an account? </label>
+          <span onClick={handleRegister} style={{ color: '#007bff', cursor: 'pointer' }}>Register here</span>
+        </div>
+      </form>
+    </div>
+  </>
+  
   );
 }
 
