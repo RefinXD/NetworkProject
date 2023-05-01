8dbc1817638e6c21d7d9904bfeb5691dcaf5a546
NarongdaetData <6330146421@student.chula.ac.th>
2023-05-01 15:44:50 +0700

    merge conflic to main

--- client/components/chat/chat.js
+++ client/components/chat/chat.js
@@ -1,16 +1,18 @@
-import styles from './styles.module.css';
-import RoomAndUsersColumn from './room-and-users'; // Add this
-import SendMessage from './send-message';
-import MessagesReceived from './messages';
-import socket from '../../utils/Utils'; // Add this
+import styles from "./styles.module.css";
+import RoomAndUsersColumn from "./room-and-users"; // Add this
+import SendMessage from "./send-message";
+import MessagesReceived from "./messages";
+import socket from "../../utils/Utils"; // Add this
 
-
-const Chat = ({ nickname, room}) => { // Add this -- our server will run on port 4000, so we connect to it from here
-  socket.emit("join_room", { username:nickname, room });
-  console.log("Chat page    ",nickname,"    ",room )
+const Chat = ({ nickname, room }) => {
+  // Add this -- our server will run on port 4000, so we connect to it from here
+  socket.emit("join_room", { username: nickname, room });
+  console.log("Chat page    ", nickname, "    ", room);
   return (
-    <div className={styles.chatContainer} style={{backgroundColor: '#E6E3D8'}}  >
-      
+    <div
+      className={styles.chatContainer}
+      style={{ backgroundColor: "#E6E3D8" }}
+    >
       <RoomAndUsersColumn socket={socket} username={nickname} room={room} />
 
       <div>
