cd2c11c5c657c747dda3e92fe631ece4b4064135
Akkharawat <akkharawatbct@gmail.com>
2023-05-03 09:24:06 +0700

    add default path to login page

---
+++ client/pages/index.js
@@ -0,0 +1,9 @@
+import { AuthProvider, useAuth } from "../components/AuthProvider";
+import Login from "../components/login/loginform";
+export default function Landing() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
